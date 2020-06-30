f790fff74cdde79471e9cde3ab1b3365ba72588e
rohanmashiyava <Rohan1611>
2020-07-01 01:26:25 +0530

    update for working project for calibri-api

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'osMsmvrJ6cve9N6WuEr6',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
