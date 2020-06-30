8175c93acdfc16f41fb1d892e3086c92fb70b85c
rohanmashiyava <Rohan1611>
2020-07-01 03:02:58 +0530

    update for working project for calibri-api

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=calibri-api',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root1234',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
